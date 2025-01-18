25a876d11f609b933c628887cc25a0d1380c4ea8
Jared Ryan <jared@mantl.com>
2025-01-18 09:41:35 -0800

    [feat]: Google Maps now shows Dad office. Now just need to make it look nice

--- app/ui/google-maps-address.tsx
+++ app/ui/google-maps-address.tsx
@@ -1,16 +1,30 @@
-import { APIProvider, Map, MapCameraChangedEvent } from '@vis.gl/react-google-maps';
+import {
+    AdvancedMarker,
+    APIProvider,
+    Map,
+    MapCameraChangedEvent,
+    Pin
+} from '@vis.gl/react-google-maps';
 
 export default function GoogleMapsAddress() {
     return (
         <APIProvider apiKey={'AIzaSyBFT4isOBiPPTttkihHnsl9WboOWUc-ZeY'} onLoad={() => console.log('Maps API has loaded.')}>
             <Map
                 defaultZoom={13}
-                defaultCenter={ { lat: -33.860664, lng: 151.208138 } }
+                defaultCenter={{ lat: 36.808851391005625, lng: -119.82297917748616 }}
                 onCameraChanged={(ev: MapCameraChangedEvent) =>
                     console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)
                 }
+                mapId={'DEMO_MAP_ID'}
                 style={{ width: '800px', height: '800px' }}
-            />
+            >
+                <AdvancedMarker
+                    key="office"
+                    position={{ lat: 36.808851391005625, lng: -119.82297917748616 }}
+                >
+                    <Pin background={'#FBBC04'} glyphColor={'#000'} borderColor={'#000'} />
+                </AdvancedMarker>
+            </Map>
         </APIProvider>
     )
 }
