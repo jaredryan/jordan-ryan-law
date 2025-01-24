e77893216b26d515aa971b6c0f509346936d97d3
Jared Ryan <jared@mantl.com>
2025-01-23 18:52:09 -0800

    [feat]: make navbar mobile friendly

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -3,9 +3,14 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { usePathname } from 'next/navigation';
+import React, { useEffect, useRef, useState } from 'react'
+import { CSSTransition } from 'react-transition-group';
 import '@/app/ui/navbar.css';
 
 import logoWhite from '@/public/logo-white.webp'
+
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const homeLink = {
   name: 'Home',
@@ -31,6 +36,23 @@ const links = [{
 
 export default function Navbar() {
   const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState(false)
+  const nodeRef = useRef(null)
+  const barsRef = useRef(null)
+
+  useEffect(() => {
+      const closeMenuOnOutsideClick = (e: { target: string })=> {
+          // @ts-ignore — weird "window" code in React
+          if (isOpen && !nodeRef.current?.contains(e.target) && !barsRef.current?.contains(e.target)) {
+              setIsOpen(false)
+          }
+      }
+
+      // @ts-ignore — weird "window" code in React
+      window.addEventListener('mousedown', closeMenuOnOutsideClick);
+      // @ts-ignore — weird "window" code in React
+      return () => window.removeEventListener('mousedown', closeMenuOnOutsideClick);
+  }, [isOpen]);
 
   return (
     <nav>
@@ -51,7 +73,7 @@ export default function Navbar() {
             Ryan Legal, PC
           </Link>
         </div>
-        <div className='menu'>
+        <div className="desktopMenu">  
           {links.map((link) => {
             return (
               <Link
@@ -63,6 +85,26 @@ export default function Navbar() {
               </Link>
             );
           })}
+        </div>
+        <div onClick={() => setIsOpen(!isOpen)} className={`barsContainer ${isOpen ? "open" : "closed"}`}>
+          <div ref={barsRef} className="barsIconContainer">
+            <FontAwesomeIcon icon={faBars} />
+          </div>
+          <CSSTransition nodeRef={nodeRef} in={isOpen} mountOnEnter unmountOnExit timeout={500} classNames="fade-bounce-down">
+              <div ref={nodeRef} className={`dropdownContent ${isOpen ? "open" : "closed"}`}>
+                {links.map((link) => {
+                  return (
+                    <Link
+                      key={link.name}
+                      href={link.href}
+                      className={`${pathname === link.href ? 'highlighted' : ''}`}
+                    >
+                      {link.name}
+                    </Link>
+                  );
+                })}
+              </div>
+          </CSSTransition>
         </div>
       </div>
     </nav>
