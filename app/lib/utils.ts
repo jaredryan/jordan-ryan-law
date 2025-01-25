accd0b626a892bdc739ca237eb8593a04afac2f4
Jared Ryan <jared@mantl.com>
2025-01-25 10:08:00 -0800

    [feat]: built contact us form and sendgrid basic functionality...just need to polish a bit

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,17 +1,102 @@
-export async function sendContactUsEmail() {
+'use server'
+
+import { z } from 'zod';
+import sgMail from '@sendgrid/mail'
+
+export type State = {
+  errors?: {
+    name?: string[];
+    phone?: string[];
+    email?: string[];
+    description?: string[];
+    sendGrid?: any;
+  };
+  message?: string | null;
+};
+
+const FormSchema = z.object({
+  name: z.string({
+    required_error: 'Name is required',
+  }),
+  phone: z.string({
+    required_error: 'Phone is required',
+  }),
+  email: z.string({
+    required_error: 'Email is required',
+  }),
+  description: z.string({
+    required_error: 'Description is required',
+  }),
+  date: z.string(),
+});
+
+const SendEmail = FormSchema.omit({ date: true });
+
+export async function sendContactUsEmail(_prevState: State, formData: FormData) {
   try {
     // We artificially delay a response for demo purposes.
     // Don't do this in production :)
+    
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+
+    // Format Email from FormData
+
+    const validatedFields = SendEmail.safeParse({
+      name: formData.get("name"),
+      phone: formData.get("phone"),
+      email: formData.get("email"),
+      description: formData.get("description"),
+    });
+  
+    if (!validatedFields.success) {
+      return {
+        errors: validatedFields.error.flatten().fieldErrors,
+        message: "Missing Fields. Failed to Create Invoice.",
+      };
+    }
+
+    const { name, phone, email, description } = validatedFields.data;
+    const jsDate = new Date()
+    const date = jsDate.toLocaleDateString();
+    const time = jsDate.toLocaleTimeString();
+
+    console.log({
+      name,
+      phone,
+      email,
+      description,
+      date,
+      time,
+    })
+
+    // Send Email
+
     console.log('Sending Contact Us Email');
 
-    // Leave this here for tasting loading code
-    await new Promise((resolve) => setTimeout(resolve, 3000));
-  
-    console.log('Replace this with SendGrid send email code');
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY as string)
+
+    const msg = {
+      to: 'jryantennis@gmail.com', // Change to your recipient
+      from: 'jryantennis@gmail.com', // Change to your verified sender
+      subject: 'Sending with SendGrid is Fun',
+      text: 'and easy to do anywhere, even with Node.js',
+      html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    }
+
+    await sgMail.send(msg)
  
-    return null
+    return {
+      message: 'success',
+      errors: {}
+    }
   } catch (error) {
     console.error('SendGrid Error:', error);
-    return new Error('Failed to send Contact Us email.');
+
+    return {
+      message: 'failed',
+      errors: {
+        sendGrid: error
+      }
+    }
   }
 }
--- app/ui/contact-us-form.tsx
+++ app/ui/contact-us-form.tsx
@@ -1,8 +1,18 @@
+'use client';
+
+import { useActionState } from 'react';
+import { sendContactUsEmail, State } from '@/app/lib/utils';
 import '@/app/ui/contact-us-form.css';
 
 export default function Form() {
+  const initialState: State = { message: null, errors: {} };
+  // @ts-ignore
+  const [state, formAction] = useActionState(sendContactUsEmail, initialState);
+
+  console.log(state)
+
   return (
-    <form className="contactUsForm">
+    <form className="contactUsForm" action={formAction}>
       <div className="inputFields">
         <div className="input">
           <label htmlFor="name">
@@ -13,6 +23,14 @@ export default function Form() {
             name="name"
             type="text"
           />
+          {/* @ts-ignore */}
+          {state && state.errors?.name && state.errors.name.map((error: string) => (
+            <div id="name-error" aria-live="polite" aria-atomic="true">
+              <p key={error}>
+                {error}
+              </p>
+            </div>
+          ))}
         </div>
         <div className="input">
           <label htmlFor="email">
@@ -22,8 +40,16 @@ export default function Form() {
             id="phone"
             name="phone"
             type="tel"
-            pattern="[0-9]{3}[0-9]{4}[0-9]{4}"
+            // pattern="[0-9]{3}[0-9]{4}[0-9]{4}"
           />
+          {/* @ts-ignore */}
+          {state && state.errors?.phone && state.errors.phone.map((error: string) => (
+            <div id="phone-error" aria-live="polite" aria-atomic="true">
+              <p key={error}>
+                {error}
+              </p>
+            </div>
+          ))}
         </div>
         <div className="input">
           <label htmlFor="email">
@@ -34,6 +60,14 @@ export default function Form() {
             name="email"
             type="email"
           />
+          {/* @ts-ignore */}
+          {state && state.errors?.email && state.errors.email.map((error: string) => (
+            <div id="email-error" aria-live="polite" aria-atomic="true">
+              <p key={error}>
+                {error}
+              </p>
+            </div>
+          ))}
         </div>
         <div className="textareaInput">
           <label htmlFor="description">
@@ -44,6 +78,14 @@ export default function Form() {
             name="description"
             rows={3}
           />
+          {/* @ts-ignore */}
+          {state && state.errors?.description && state.errors.description.map((error: string) => (
+            <div id="description-error" aria-live="polite" aria-atomic="true">
+              <p key={error}>
+                {error}
+              </p>
+            </div>
+          ))}
         </div>
       </div>
       <button type="submit">Submit</button>
