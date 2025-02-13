ffe42bbfa589437909e719f21fb06b69ebeef96f
Jared Ryan <jared@mantl.com>
2025-02-13 15:14:58 -0800

    [chore]: got next analyze working, some package cleanup

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,15 @@
 import type { NextConfig } from 'next'
+import nextBundleAnalyzer from '@next/bundle-analyzer'
 
-const nextConfig: NextConfig = {
+const withBundleAnalyzer = nextBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+const nextConfig: NextConfig = withBundleAnalyzer({
   /* config options here */
   env: {
     NEXT_PUBLIC_CURRENT_URL: process.env.NEXT_PUBLIC_CURRENT_URL || "https://ryanlegalpc.com",
   },
-}
+})
 
 export default nextConfig
